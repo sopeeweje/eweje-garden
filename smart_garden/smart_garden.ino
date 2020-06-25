30c6965e4cc21fef046f11409d8dcd2662b0a790
Sope Eweje <feyisope.eweje@pennmedicine.upenn.edu>
2020-06-24 20:00:52 -0400

    initialize

---
+++ smart_garden/smart_garden.ino
@@ -0,0 +1,95 @@
+#include <FirebaseCloudMessaging.h>
+#include <Firebase.h>
+#include <FirebaseHttpClient.h>
+#include <FirebaseArduino.h>
+#include <FirebaseError.h>
+#include <FirebaseObject.h>
+#include <ESP8266WiFi.h>
+#include <FirebaseAuth.h> //authentication token defined in separate private file (FIREBASE_AUTH)
+ 
+// Set these to run example. 
+#define FIREBASE_HOST "eweje-garden.firebaseio.com"
+#define WIFI_SSID "744 S 19th St Apt 2 (5GHz)" 
+#define WIFI_PASSWORD "eLN271828!" 
+ 
+void setup() { 
+  Serial.begin(9600); 
+ 
+  // connect to wifi. 
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD); 
+  Serial.print("connecting"); 
+  while (WiFi.status() != WL_CONNECTED) { 
+    Serial.print("."); 
+    delay(500); 
+  } 
+  Serial.println(); 
+  Serial.print("connected: "); 
+  Serial.println(WiFi.localIP()); 
+   
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH); 
+} 
+ 
+int n = 0; 
+ 
+void loop() { 
+  // set value 
+  Firebase.setFloat("number", 42.0); 
+  // handle error 
+  if (Firebase.failed()) { 
+      Serial.print("setting /number failed:"); 
+      Serial.println(Firebase.error());   
+      return; 
+  } 
+  delay(1000); 
+   
+  // update value 
+  Firebase.setFloat("number", 43.0); 
+  // handle error 
+  if (Firebase.failed()) { 
+      Serial.print("setting /number failed:"); 
+      Serial.println(Firebase.error());   
+      return; 
+  } 
+  delay(1000); 
+ 
+  // get value  
+  Serial.print("number: "); 
+  Serial.println(Firebase.getFloat("number")); 
+  delay(1000); 
+ 
+  // remove value 
+  Firebase.remove("number"); 
+  delay(1000); 
+ 
+  // set string value 
+  Firebase.setString("message", "hello world"); 
+  // handle error 
+  if (Firebase.failed()) { 
+      Serial.print("setting /message failed:"); 
+      Serial.println(Firebase.error());   
+      return; 
+  } 
+  delay(1000); 
+   
+  // set bool value 
+  Firebase.setBool("truth", false); 
+  // handle error 
+  if (Firebase.failed()) { 
+      Serial.print("setting /truth failed:"); 
+      Serial.println(Firebase.error());   
+      return; 
+  } 
+  delay(1000); 
+ 
+  // append a new value to /logs 
+  String name = Firebase.pushInt("logs", n++); 
+  // handle error 
+  if (Firebase.failed()) { 
+      Serial.print("pushing /logs failed:"); 
+      Serial.println(Firebase.error());   
+      return; 
+  } 
+  Serial.print("pushed: /logs/"); 
+  Serial.println(name); 
+  delay(1000); 
+}
