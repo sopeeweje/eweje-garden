01f6a59494bd6404605cfa91cd750daacf1dc7c4
Sope Eweje <feyisope.eweje@pennmedicine.upenn.edu>
2020-06-28 19:41:24 -0400

    initialized new web app

---
+++ react-firebase_eweje-garden/src/components/App/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import Graph from "../Graph/Graph"
+import Dropdown from "../Dropdown/Dropdown"
+import './App.css';
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <Dropdown></Dropdown>
+        <Graph sensor="humidity" />
+        <Graph sensor="moisture" />
+        <Graph sensor="temp" />
+        <Graph sensor="light" />
+      </div>
+    )
+  }
+}
+
+export default App;
