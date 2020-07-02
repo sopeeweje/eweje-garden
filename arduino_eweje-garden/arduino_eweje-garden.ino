b1c41883aa58e25d3a9d05a0b69ac9ac38f95344
Sope Eweje <feyisope.eweje@pennmedicine.upenn.edu>
2020-07-02 11:14:33 -0400

    Sending sensor data with date and time

--- arduino_eweje-garden/arduino_eweje-garden.ino
+++ arduino_eweje-garden/arduino_eweje-garden.ino
@@ -5,17 +5,39 @@
 #include <FirebaseError.h>
 #include <FirebaseObject.h>
 #include <ESP8266WiFi.h>
-#include <Time.h>
+#include <TimeLib.h>
+#include <time.h>
+#include <DS1307RTC.h>  // a basic DS1307 library that returns time as a time_t
+#include <Adafruit_TSL2591.h> //Light sensor
+#include <Adafruit_Si7021.h> //temp and humidity sensor
+#include <Adafruit_miniTFTWing.h>
+#include <seesaw_servo.h>
+#include <Adafruit_Crickit.h>
+#include <seesaw_neopixel.h>
+#include <Adafruit_TFTShield18.h>
+#include <seesaw_motor.h>
+#include <Adafruit_NeoTrellis.h>
+#include <Adafruit_seesaw.h>
+#include <ArduinoJson.h>
 
 #include "FirebaseAuth.h"
 //authentication token defined in separate private file (FIREBASE_AUTH)
 //wifi password defined in separate file (WIFI_PASSWORD)
 
-//#include <ArduinoJson.h>
-
 // Set these to run example. 
 #define FIREBASE_HOST "eweje-garden.firebaseio.com"
-#define WIFI_SSID "744 S 19th St Apt 2 (5GHz)" 
+#define WIFI_SSID "744 S 19th St Apt 2 (2.4GHz)"
+
+Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591);
+Adafruit_Si7021 si7021 = Adafruit_Si7021();
+Adafruit_seesaw ss;
+char sensorName[8] = "Sensor1";
+uint16_t light;
+float temperature;
+float humidity;
+float soilTemp;
+uint16_t soilMoisture;
+int counter = 0;
  
 void setup() { 
   Serial.begin(9600); 
@@ -30,71 +52,77 @@ void setup() {
   Serial.println(); 
   Serial.print("connected: "); 
   Serial.println(WiFi.localIP()); 
-   
+
+  configTime(4 * 3600, 0, "pool.ntp.org", "time.nist.gov"); //(int timezoneinseconds, int daylightOffset_sec, const char* server1, const char* server2, const char* server3)
+  Serial.println("\nWaiting for time");
+  while (!time(nullptr)) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("");
+
+  tsl.setGain(TSL2591_GAIN_MED);      // 25x gain 
+  tsl.setTiming(TSL2591_INTEGRATIONTIME_300MS);
+  si7021.begin();
+  si7021.heater(false);
+  ss.begin(0x36);
+  pinMode(12, OUTPUT);
+  digitalWrite(12, LOW);
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH); 
 } 
  
-int n = 0; 
- 
-void loop() { 
-  // set value 
-  Firebase.setFloat("number", 42.0); 
-  // handle error 
-  if (Firebase.failed()) { 
-      Serial.print("setting /number failed:"); 
-      Serial.println(Firebase.error());   
-      return; 
-  } 
-  delay(1000); 
-   
-  // update value 
-  Firebase.setFloat("number", 43.0); 
-  // handle error 
-  if (Firebase.failed()) { 
-      Serial.print("setting /number failed:"); 
-      Serial.println(Firebase.error());   
-      return; 
-  } 
-  delay(1000); 
- 
-  // get value  
-  Serial.print("number: "); 
-  Serial.println(Firebase.getFloat("number")); 
-  delay(1000); 
- 
-  // remove value 
-  Firebase.remove("number"); 
-  delay(1000); 
- 
-  // set string value 
-  Firebase.setString("message", "hello world"); 
-  // handle error 
-  if (Firebase.failed()) { 
-      Serial.print("setting /message failed:"); 
-      Serial.println(Firebase.error());   
-      return; 
-  } 
-  delay(1000); 
-   
-  // set bool value 
-  Firebase.setBool("truth", false); 
-  // handle error 
-  if (Firebase.failed()) { 
-      Serial.print("setting /truth failed:"); 
-      Serial.println(Firebase.error());   
-      return; 
-  } 
-  delay(1000); 
- 
-  // append a new value to /logs 
-  String name = Firebase.pushInt("logs", n++); 
+void loop() {
+  StaticJsonBuffer<200> dataPoint_buffer;
+  JsonObject& dataPoint = dataPoint_buffer.createObject();
+  
+  time_t now = time(nullptr);
+  String dateTime = ctime(&now); //Wed Jul  1 11:43:53 2020
+  light = tsl.getLuminosity(TSL2591_FULLSPECTRUM);
+  temperature = 1.8*si7021.readTemperature()+32;
+  humidity = si7021.readHumidity();
+  if (counter == 600){
+    digitalWrite(12, HIGH);
+    delay(1000);
+    soilTemp = ss.getTemp();
+    soilMoisture = ss.touchRead(0);
+    digitalWrite(12, LOW);
+    counter = 0;
+    dataPoint["soil_temp"] = 1.8*soilTemp+32;
+    dataPoint["soil_moisture"] = soilMoisture;
+  }
+  else{
+    dataPoint["soil_temp"] = "-";
+    dataPoint["soil_moisture"] = "-";
+  }
+  
+  dataPoint["date"] = getDate(dateTime);
+  dataPoint["time"] = getTime(dateTime);
+  dataPoint["sensor"] = sensorName;
+  dataPoint["light"] = light;
+  dataPoint["temp"] = temperature;
+  dataPoint["humidity"] = humidity;
+  
+  dataPoint.printTo(Serial);
+  Serial.println("");
+  Firebase.push("data", dataPoint); 
   // handle error 
   if (Firebase.failed()) { 
       Serial.print("pushing /logs failed:"); 
       Serial.println(Firebase.error());   
       return; 
   } 
-  Serial.print("pushed: /logs/"); 
-  Serial.println(name); 
-  delay(1000); 
+  counter++;
+  delay(1000);
+}
+
+String getDate(String cdt){
+  String currentDate = cdt.substring(4,7)+'-'+cdt.substring(8,10)+'-'+cdt.substring(20,24);
+  currentDate.replace(" ", "");
+  return currentDate;
+}
+
+String getTime(String cdt){
+  String currentTime = cdt.substring(11,19);
+  currentTime.replace(":", "");
+  return currentTime;
 }
