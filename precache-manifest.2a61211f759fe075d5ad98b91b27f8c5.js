1385a5dcd866b1cead49189e5d4b3f521545055a
Sope Eweje <feyisope.eweje@pennmedicine.upenn.edu>
2020-07-28 20:49:12 -0400

    Updates

---
+++ precache-manifest.2a61211f759fe075d5ad98b91b27f8c5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e11630cb53c3576fdf1b9d628a300bf",
+    "url": "/eweje-garden/index.html"
+  },
+  {
+    "revision": "2ca0ae0984a2a87b0c27",
+    "url": "/eweje-garden/static/css/main.8c0390ef.chunk.css"
+  },
+  {
+    "revision": "96d6528877f23a60dbf4",
+    "url": "/eweje-garden/static/js/2.17be881e.chunk.js"
+  },
+  {
+    "revision": "665ec99afa88d2424080b53ebb3270e5",
+    "url": "/eweje-garden/static/js/2.17be881e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ca0ae0984a2a87b0c27",
+    "url": "/eweje-garden/static/js/main.d9b5b3b9.chunk.js"
+  },
+  {
+    "revision": "3da1e023e7155cd33a3a",
+    "url": "/eweje-garden/static/js/runtime-main.34770e11.js"
+  },
+  {
+    "revision": "03e91f122aa5fd425abbe23c85546eb0",
+    "url": "/eweje-garden/static/media/Linearicons-Free.03e91f12.woff2"
+  },
+  {
+    "revision": "2f3e9f80fff7d699dd3de6904d7d1647",
+    "url": "/eweje-garden/static/media/Linearicons-Free.2f3e9f80.ttf"
+  },
+  {
+    "revision": "65060723fe964f85afa0a82d0bb78cf9",
+    "url": "/eweje-garden/static/media/Linearicons-Free.65060723.woff"
+  },
+  {
+    "revision": "71ad32ce1ab07350277dfcf1f7a503a5",
+    "url": "/eweje-garden/static/media/Linearicons-Free.71ad32ce.svg"
+  },
+  {
+    "revision": "b9b7f23cb61b1f503e1249b63d980448",
+    "url": "/eweje-garden/static/media/Linearicons-Free.b9b7f23c.eot"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/eweje-garden/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/eweje-garden/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/eweje-garden/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/eweje-garden/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "f641336995bf50b237e168a196c7381d",
+    "url": "/eweje-garden/static/media/banner.f6413369.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/eweje-garden/precache-manifest.2a61211f759fe075d5ad98b91b27f8c5.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/eweje-garden/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
