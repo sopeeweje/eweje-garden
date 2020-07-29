3ec2aeaa417537f02b6064d7d2cbe71d6b45d958
Sope Eweje <feyisope.eweje@pennmedicine.upenn.edu>
2020-07-28 20:34:34 -0400

    Authorization

--- react-firebase_eweje-garden/src/components/Firebase/Firebase.js
+++ react-firebase_eweje-garden/src/components/Firebase/Firebase.js
@@ -2,4 +2,6 @@ import Firebase from "firebase";
 import FirebaseConfig from './FirebaseConfig';
 
 const app = Firebase.initializeApp(FirebaseConfig);
+
+export const auth = app.auth();
 export const db = app.database();
